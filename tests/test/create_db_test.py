3aa6593e06c9303cc4a99131ee767c6b3945323c
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-02-12 15:57:49 +0100

    Added testing

---
+++ tests/test/create_db_test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+'''module to test the creation of a cluster database'''
+
+import os, sqlite3, unittest
+import create_node_db
+
+class CreateNodeDbTest(unittest.TestCase):
+    '''Tests the creation of a cluster database'''
+
+    def setUp(self):
+        self._file_name = 'data/nodes.db'
+        try:
+            os.remove(self._file_name)
+        except OSError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove(self._file_name)
+        except OSError:
+            pass
+
+    def test_create(self):
+        tables = {'partitions', 'nodes', 'properties', 'features',
+                  'qos_levels'}
+        with sqlite3.connect(self._file_name) as conn:
+            create_node_db.init_db(conn, create_node_db.DB_DESC)
+        cursor = conn.cursor()
+        result = cursor.execute("""SELECT name FROM sqlite_master
+                                       WHERE type = 'table'""")
+        nr_rows = 0
+        for row in result:
+            if not row[0].startswith('sqlite_'):
+                self.assertIn(row[0], tables)
+                nr_rows += 1
+        self.assertEquals(len(tables), nr_rows)
---
+++ tests/test/load_db_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+'''module to test the loading of a cluster database'''
+
+import json, os, sqlite3, unittest
+import create_node_db
+import load_node_db
+from vsc.pbs.pbsnodes import PbsnodesParser
+
+class LoadNodeDbTest(unittest.TestCase):
+    '''Tests the loading of a cluster database'''
+
+    def setUp(self):
+        self._file_name = 'data/nodes.db'
+        try:
+            os.remove(self._file_name)
+        except OSError:
+            pass
+        with sqlite3.connect(self._file_name) as conn:
+            create_node_db.init_db(conn, create_node_db.DB_DESC)
+        config_file_name = '../../../vsc-tools-lib/conf/config.json'
+        with open(config_file_name, 'r') as config_file:
+            self._config = json.load(config_file)
+
+    def tearDown(self):
+        try:
+            os.remove(self._file_name)
+        except OSError:
+            pass
+
+    def test_load(self):
+        pbsnodes_file_name = 'data/pbsnodes.txt'
+        qos_levels = self._config['qos_levels']
+        partitions = self._config['partitions']
+        with open(pbsnodes_file_name, 'r') as pbsnode_file:
+            pbsnodes_parser = PbsnodesParser()
+            nodes = pbsnodes_parser.parse_file(pbsnode_file)
+        with sqlite3.connect(self._file_name) as conn:
+            load_node_db.insert_partitions(conn, partitions)
+            load_node_db.insert_qos_levels(conn, qos_levels)
+            load_node_db.insert_node_info(conn, nodes, partitions)
