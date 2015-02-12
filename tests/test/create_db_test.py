6b98f7e401a04ed47e4fc8d1fa976c86af409f01
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-02-12 16:25:05 +0100

    Fixed code context

--- tests/test/create_db_test.py
+++ tests/test/create_db_test.py
@@ -25,12 +25,12 @@ class CreateNodeDbTest(unittest.TestCase):
                   'qos_levels'}
         with sqlite3.connect(self._file_name) as conn:
             create_node_db.init_db(conn, create_node_db.DB_DESC)
-        cursor = conn.cursor()
-        result = cursor.execute("""SELECT name FROM sqlite_master
-                                       WHERE type = 'table'""")
-        nr_rows = 0
-        for row in result:
-            if not row[0].startswith('sqlite_'):
-                self.assertIn(row[0], tables)
-                nr_rows += 1
-        self.assertEquals(len(tables), nr_rows)
+            cursor = conn.cursor()
+            result = cursor.execute("""SELECT name FROM sqlite_master
+                                           WHERE type = 'table'""")
+            nr_rows = 0
+            for row in result:
+                if not row[0].startswith('sqlite_'):
+                    self.assertIn(row[0], tables)
+                    nr_rows += 1
+            self.assertEquals(len(tables), nr_rows)
