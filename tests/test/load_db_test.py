b23995b5969b30a4ec3f99802e36741b139d69cc
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-02-12 21:49:55 +0100

    Added more tests

--- tests/test/load_db_test.py
+++ tests/test/load_db_test.py
@@ -31,15 +31,38 @@ class LoadNodeDbTest(unittest.TestCase):
         pbsnodes_file_name = 'data/pbsnodes.txt'
         qos_levels = self._config['qos_levels']
         partitions = self._config['partitions']
+        error_msg = 'E: node r3i0n2 has no status\n'
+        nr_nodes = 163
+        nr_128gb_nodes = 32
+        nr_ivybridge_nodes = 143
         with open(pbsnodes_file_name, 'r') as pbsnode_file:
             pbsnodes_parser = PbsnodesParser()
             nodes = pbsnodes_parser.parse_file(pbsnode_file)
         with sqlite3.connect(self._file_name) as conn:
             load_node_db.insert_partitions(conn, partitions)
             load_node_db.insert_qos_levels(conn, qos_levels)
-            error = sys.stderr
+            stderr_tmp = sys.stderr
             sys.stderr = StringIO.StringIO()
             load_node_db.insert_node_info(conn, nodes, partitions)
-            print 'my error', sys.stderr.get_value()
-            sys.stderr = error
+            self.assertEquals(error_msg, sys.stderr.getvalue())
+            sys.stderr = stderr_tmp
             cursor = conn.cursor()
+            result = cursor.execute('''SELECT qos FROM qos_levels''')
+            nr_qos_levels = 0
+            for row in result:
+                self.assertIn(row[0], qos_levels)
+                nr_qos_levels += 1
+            self.assertEquals(len(qos_levels), nr_qos_levels)
+            result = cursor.execute('''SELECT partition_name
+                                           FROM partitions''')
+            nr_partitions = 0
+            for row in result:
+                self.assertIn(row[0], partitions)
+                nr_partitions += 1
+            self.assertEquals(len(partitions), nr_partitions)
+            result = cursor.execute("""SELECT count(*) FROM features
+                                           WHERE feature = 'mem128'""")
+            self.assertEquals(nr_128gb_nodes, result.fetchone()[0])
+            result = cursor.execute("""SELECT count(*) FROM properties
+                                           WHERE property = 'ivybridge'""")
+            self.assertEquals(nr_ivybridge_nodes, result.fetchone()[0])
